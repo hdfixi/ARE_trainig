fd31c8114075b25684d361d6d6f6d2b7c69f6a89
hdfixi <hdfixi0000@gmail.com>
2024-02-20 21:52:21 +0100

    C file updated

--- are.cpp
+++ are.cpp
@@ -1,5 +1,1 @@
 #include<bits/stdc++.h>
-using namespace std ;
-int main(){
-    cout<<"salmou alaikom !"<<endl; 
-}
---
+++ chiheb.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+
+int main(){
+    printf("chiheb bara raw7");
+}
